92444896f21ba90d8a6bd1d1a6a08ec03e2330ab
Kae <kaehehehe@gmail.com>
2022-02-03 01:44:54 +0900

    :lipstick: Fix toggle styling code

--- custom-component/src/component/Toggle.js
+++ custom-component/src/component/Toggle.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import '../style/toggle.css';
 import { Container } from '../style/container';
 import { Title } from '../style/title';
 
@@ -15,6 +14,64 @@ const ToggleContainer = styled(Container)`
 
 const ToggleTitle = styled(Title)`
   color: ${({ theme }) => (theme === 'dark' ? 'white' : 'black')};
+`;
+
+const StyledToggle = styled.label`
+  position: relative;
+  display: inline-block;
+  width: 65px;
+  height: 32px;
+  cursor: pointer;
+`;
+
+const LightModeIcon = styled.div`
+  display: ${({ theme }) => (theme === 'dark' ? 'block' : 'none')};
+  position: absolute;
+  font-size: 25px;
+  top: -1px;
+  left: 3px;
+  z-index: 100;
+`;
+
+const DarkModeIcon = styled(LightModeIcon)`
+  display: ${({ theme }) => (theme === 'dark' ? 'none' : 'block')};
+  left: 38px;
+`;
+
+const Input = styled.input`
+  opacity: 0;
+  width: 0;
+  height: 0;
+`;
+
+const Slider = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: ${({ theme }) =>
+    theme === 'dark' ? 'skyblue' : '#1c2128'};
+  border-radius: 32px;
+
+  &::before {
+    position: absolute;
+    content: '';
+    height: 24px;
+    width: 24px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    transition: 300ms;
+    border-radius: 50%;
+    transform: ${({ theme }) => theme === 'dark' && 'translateX(33px)'};
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    font-size: 22px;
+  }
 `;
 
 const Text = styled.div`
@@ -33,10 +90,12 @@ const Toggle = () => {
   return (
     <ToggleContainer theme={theme}>
       <ToggleTitle theme={theme}>Toggle</ToggleTitle>
-      <label className="toggle">
-        <input type="checkbox" />
-        <span className="slider" onClick={handleToggle} />
-      </label>
+      <StyledToggle>
+        <LightModeIcon theme={theme}>🌞</LightModeIcon>
+        <DarkModeIcon theme={theme}>🌛</DarkModeIcon>
+        <Input type="checkbox" onClick={handleToggle} />
+        <Slider theme={theme} />
+      </StyledToggle>
       <Text theme={theme}>{theme} mode</Text>
     </ToggleContainer>
   );
